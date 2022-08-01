d09d6c0527b02b4e4861398d2997dea9d824a9f8
Priyal Patil <preeyal.patil@gmail.com>
2022-08-01 15:19:39 +0530

    First File adding to Git

---
+++ HelloJava.java
@@ -0,0 +1,22 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2636
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fswiss\fcharset0 Helvetica-Oblique;}
+{\colortbl;\red255\green255\blue255;\red0\green29\blue164;\red9\green9\blue9;\red10\green79\blue103;
+\red17\green109\blue18;\red114\green0\blue130;}
+{\*\expandedcolortbl;;\csgenericrgb\c0\c11373\c64314;\csgenericrgb\c3529\c3529\c3529;\csgenericrgb\c3922\c30980\c40392;
+\csgenericrgb\c6667\c42745\c7059;\csgenericrgb\c44706\c0\c50980;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww28600\viewh17440\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs26 \cf2 import java.util.*;\
+\
+public class \cf0 HelloJava\
+\cf3 \{\
+    \cf2 public static void \cf4 main\cf3 (\cf0 String \cf3 args[])\
+    \{\
+        \cf0 String string_demo \cf3 = \cf5 "String"\cf3 ;\
+        \cf0 System\cf3 .
+\f1\i \cf6 out
+\f0\i0 \cf3 .println(\cf0 string_demo\cf3 );\
+    \}\
+\}\
+}
