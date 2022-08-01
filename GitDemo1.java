b3ceaefb27d11b0e240eab2e1ef5745ef556bf41
Priyal Patil <preeyal.patil@gmail.com>
2022-08-01 22:58:49 +0530

    required changes done

--- GitDemo1.java
+++ GitDemo1.java
@@ -2,7 +2,7 @@ public class GitDemo1
 {
     public static void main(String args[])
     {
-        String string_demo = "String";
+        String string_demo = "Hello World";
         System.out.println(string_demo);
     }
 }
